88d7e4970fc0f292635c4faf53a990f434492d6c
redmarss <31237613+redmarss@users.noreply.github.com>
2018-11-28 23:16:42 +0800

    test

--- myGlobal/myCls/StockCls.py
+++ myGlobal/myCls/StockCls.py
@@ -3,4 +3,21 @@
 import myGlobal.myCls.StockPriceCls as StockPriceCls
 
 class Stock(StockPriceCls.StockPrice):
-    #init函数使用父函数的
+    _listStock = list()
+
+
+
+    def __init__(self,list1):
+        if list(map(lambda x:isinstance(x,StockPriceCls.StockPrice),list1)).count(True)== len(list1):       #判断是否list1中每个元素都是StockPrice类
+            print(True)
+        else:
+            print(False)
+
+
+    def MA5(self):
+        pass
+
+
+list1=['str',11]
+s=Stock(list1)
+input()
