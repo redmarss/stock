7090eb3e9e819d3bc2ee85bc78a49b9c8aa75164
redmarss <31237613+redmarss@users.noreply.github.com>
2018-12-02 13:55:58 +0800

    test

--- Analyze/followBroker.py
+++ Analyze/followBroker.py
@@ -1,5 +1,20 @@
 #!/bin/usr/env python
 # -*- coding:utf-8 -*-
+import myGlobal.myCls.mysqlCls as msql
 
-#跟随机构，模拟买入，并统计盈利
-def followBroker(broker_code = None,ts_date = None, amount = 1000):
+#输入机构代码，日期，返回元组
+#（broker_code,ts_date,[stock_list]）
+def getStockCode(broker_code = None,ts_date = None):
+    dbObject = msql.SingletonModel(host='localhost',port='3306',user='root',passwd='redmarss',db='tushare',charset='utf8')
+    d = dbObject.fetchall(table='broker_buy_stock_info as a,broker_buy_summary as b',
+                          field='a.stock_code',
+                          where='a.broker_buy_summary_id=b.id and b.broker_code="%s" and b.ts_date="%s"' % (broker_code, ts_date))
+    wantbuy_list = [item['stock_code'][:6] for item in d]
+    return broker_code, ts_date, wantbuy_list
+
+
+def buy_stock(t):
+    #判断传入参数是否合规
+    if not isinstance(t, tuple) or not isinstance(t[2], list):
+        print("传入参数有误")
+        return
