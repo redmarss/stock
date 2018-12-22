c507a6366cab4cd2721649767b0951aa87e00c11
redmarss <31237613+redmarss@users.noreply.github.com>
2018-12-22 23:11:27 +0800

    test

--- Analyze/followBroker.py
+++ Analyze/followBroker.py
@@ -20,19 +20,24 @@ def simulate_buy(startdate='2017-01-01',enddate='2018-12-31', amount=1000):
         b.simulate_buy(amount)
 
 
-
-if __name__ =='__main__':
-    # simulate_buy("2018-11-30","2018-12-31",1000)
-    dbObject = msql.SingletonModel(host='localhost', port='3306', user='root', passwd='redmarss', db='tushare',
-                                   charset='utf8')
+def getTopBroker_avr():
+    dbObject = msql.SingletonModel(host="localhost",port="3306",user="root",passwd="redmarss",db="tushare",charset="utf8")
     t = dbObject.fetchall(table="simulate_buy",
                           field="ts_date,broker_code,stock_code,buy_price,sell_price,amount,gainmoney,gainpercent")
-    list_title = ['ts_date','broker_code','stock_code','buy_price','sell_price','amount','gainmoney','gainpercent']
-    df = pd.DataFrame(list(t),columns=list_title)
-    df=df.sort_values(axis=0,by='broker_code',ascending='False')
-    df = df.apply(pd.to_numeric, errors='ignore')
-    value=df.groupby(['broker_code'])['gainpercent'].count()
-    value = value.sort_values(ascending=False)
-    print(value)1
+
+
+if __name__ =='__main__':
+    simulate_buy("2017-01-01","2018-11-30",1000)
+    # dbObject = msql.SingletonModel(host='localhost', port='3306', user='root', passwd='redmarss', db='tushare',
+    #                                charset='utf8')
+    # t = dbObject.fetchall(table="simulate_buy",
+    #                       field="ts_date,broker_code,stock_code,buy_price,sell_price,amount,gainmoney,gainpercent")
+    # list_title = ['ts_date','broker_code','stock_code','buy_price','sell_price','amount','gainmoney','gainpercent']
+    # df = pd.DataFrame(list(t),columns=list_title)
+    # df=df.sort_values(axis=0,by='broker_code',ascending='False')
+    # df = df.apply(pd.to_numeric, errors='ignore')
+    # value=df.groupby(['broker_code'])['gainpercent'].count()
+    # value = value.sort_values(ascending=False)
+    # print(value)
 
 
