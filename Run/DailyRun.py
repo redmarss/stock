9801c95e849b026c494d6b07ba0cf8f4660094c5
redmarss <31237613+redmarss@users.noreply.github.com>
2018-12-25 23:04:27 +0800

    test

--- Run/DailyRun.py
+++ Run/DailyRun.py
@@ -5,15 +5,23 @@ import myGlobal.globalFunction as gf
 import myGlobal.myCls.mysqlCls as msql
 #每日8.50分运行
 def getStockEveryDay():
-    date = str(datetime.datetime.today().date())
+    date = str(datetime.datetime.today().date()+datetime.timedelta(days=-1))
+    date="2018-01-05"
     dbObject = msql.SingletonModel(host='localhost', port='3306',
                                          user='root', passwd='redmarss',
                                          charset='utf8', db='tushare')
+    listock=list()
     if gf.is_holiday(str(date)) == False:           #交易日
         tbroker = dbObject.fetchall(table="best_broker_list",field="broker_code")
         libroker = [i[0] for i in tbroker]
         for i in range(len(libroker)):
-            dbObject.fetchall()
+            tstock = dbObject.fetchall(table="broker_buy_stock_info as a,broker_buy_summary as b",
+                              field="stock_code",
+                              where="b.id=a.broker_buy_summary_id and broker_code='%s' and ts_date='%s'"%(libroker[i],date))
+            for j in range(len(tstock)):
+                if tstock[j] not in listock:
+                    listock.append(tstock[j][0])
+    print(listock)
 
 if __name__ == "__main__":
     getStockEveryDay()
