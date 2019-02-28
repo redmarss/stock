cf14867a890dabfd988a8e77099202f64f66fd72
redmarss <31237613+redmarss@users.noreply.github.com>
2019-03-01 00:14:25 +0800

    test

--- test.py
+++ test.py
@@ -2,13 +2,19 @@ import numpy as np
 import myGlobal.myCls.msql as msql
 import pandas as pd
 from multiprocessing.dummy import Pool as ThreadPool
+from urllib.request import urlopen, Request
+import json
 
 
+url = "https://gupiao.baidu.com/api/stocks/stockdaybar?from=pc&os_ver=1&cuid=xxx&vv=100&format=json&stock_code=sh600500&step=3&start=20170105&count=160&fq_type=no"
 
+try:
+    request = Request(url)
+    lines = urlopen(request, timeout=10).read()
+except Exception as e:
+    print(e)
 
-https://gupiao.baidu.com/api/stocks/stockdaybar?from=pc&os_ver=1&cuid=xxx&vv=100&format=json&stock_code=sh600500&step=3&start=20170105&count=160&fq_type=no
-
-
-start：日期参数，不填则为今天
-count:获取数量
-fq_type：front/back/no
+t = json.loads(lines)
+t['stock_code']="600000"
+l = json.dumps(t).encode()
+print(l)
