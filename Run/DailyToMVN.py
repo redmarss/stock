dd409fc09ca4778695c21159b96cc184bacec96b
redmarss <31237613+redmarss@users.noreply.github.com>
2019-04-01 23:54:18 +0800

    test

--- Run/DailyToMVN.py
+++ Run/DailyToMVN.py
@@ -9,54 +9,80 @@ import pymysql
 import datetime
 import myGlobal.myCls.msql as msql
 import time
+import re
 
 # 定义复权3种模式
 list_fq = ['no', 'front', 'back']
 # 获取所有股票列表
 stock_li = gf.getAllStock(where="no_flag=0 or front_flag=0 or back_flag=0")
 def _getDayData(stock_code, date, filetype, count, list_fq): #fqtype作为多线程参数一定要放第一个
-    '''
-    获取股票日线数据及技术指标，分别存入对应的三张表中
-    :param fqtype: 复权参数【'no','front','back'】
-    :param stock_code_list: 所要操作的股票代码列表
-    :param date: 取该日期前的数据
-    :param filetype: json
-    :param count: 获取数据天数
-    flag : 0:还未处理数据，1：正常处理数据，2：出错数据
-    :return:
-    '''
-    conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='redmarss', db='tushare',
-                                   charset='utf8')
-    cur = conn.cursor()
-    for _ in range(3):          #为防止网络lag，循环3次
-        for fqtype in list_fq:
-            url = "https://gupiao.baidu.com/api/stocks/stockdaybar?from=pc&os_ver=1&cuid=xxx&vv=100&format=%s&stock_code=%s&step=3&start=%s&count=%s&fq_type=%s" \
-                  % (filetype,stock_code,date,count,fqtype)
-            try:
-                request = Request(url)
-                lines = urlopen(request, timeout=10).read()
-            except Exception as e:
-                print(e)
-                return
-            else:
-                if len(lines)<50:
-                    print("无法取得%s股票相关数据" % stock_code)
-                    sql = "update stock_basic_table set %s_flag='2' where stockcode='%s'" % (fqtype, stock_code)
-                    cur.execute(sql)
-                    conn.commit()
-                    return
-                else:
-                    t = json.loads(lines)
-                    t['stock_code'] = stock_code
-                    rbyte = json.dumps(t).encode()
-            urlPost = "http://localhost:8080/stock/detail/%s" % fqtype
-            gf.postData(rbyte,urlPost,flag="daily")
-            sql = "update stock_basic_table set %s_flag='1' where stockcode='%s'" % (fqtype, stock_code)
-            print("股票代码(%s)%s复权数据完成" % (stock_code,fqtype))
-            cur.execute(sql)
-            conn.commit()
-    cur.close()
-
+    # '''
+    # 获取股票日线数据及技术指标，分别存入对应的三张表中
+    # :param fqtype: 复权参数【'no','front','back'】
+    # :param stock_code_list: 所要操作的股票代码列表
+    # :param date: 取该日期前的数据
+    # :param filetype: json
+    # :param count: 获取数据天数
+    # flag : 0:还未处理数据，1：正常处理数据，2：出错数据
+    # :return:
+    # '''
+    # conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='redmarss', db='tushare',
+    #                                charset='utf8')
+    # cur = conn.cursor()
+    # for _ in range(3):          #为防止网络lag，循环3次
+    #     for fqtype in list_fq:
+    #         url = "https://gupiao.baidu.com/api/stocks/stockdaybar?from=pc&os_ver=1&cuid=xxx&vv=100&format=%s&stock_code=%s&step=3&start=%s&count=%s&fq_type=%s" \
+    #               % (filetype,stock_code,date,count,fqtype)
+    #         try:
+    #             request = Request(url)
+    #             lines = urlopen(request, timeout=10).read()
+    #         except Exception as e:
+    #             print(e)
+    #             return
+    #         else:
+    #             if len(lines)<50:
+    #                 print("无法取得%s股票相关数据" % stock_code)
+    #                 sql = "update stock_basic_table set %s_flag='2' where stockcode='%s'" % (fqtype, stock_code)
+    #                 cur.execute(sql)
+    #                 conn.commit()
+    #                 return
+    #             else:
+    #                 t = json.loads(lines)
+    #                 t['stock_code'] = stock_code
+    #                 rbyte = json.dumps(t).encode()
+    #         urlPost = "http://localhost:8080/stock/detail/%s" % fqtype
+    #         gf.postData(rbyte,urlPost,flag="daily")
+    #         sql = "update stock_basic_table set %s_flag='1' where stockcode='%s'" % (fqtype, stock_code)
+    #         print("股票代码(%s)%s复权数据完成" % (stock_code,fqtype))
+    #         cur.execute(sql)
+    #         conn.commit()
+    # cur.close()
+    def getDayData(code=None, start="2017-01-01", end="2018-12-31"):
+        symbol = ct._code_to_symbol(code)  # 将代码转换成标准格式
+        url = 'http://web.ifzq.gtimg.cn/appstock/app/fqkline/get?_var=kline_dayqfq2017&param=%s,day,%s,%s,640,qfq' % (
+        symbol, start, end)
+        try:
+            request = Request(url)
+            lines = urlopen(request, timeout=10).read()
+            if len(lines) < 100:  # no data
+                return None
+        except Exception as e:
+            print(e)
+        else:
+            lines = lines.decode('utf-8') if ct.PY3 else lines
+            lines = lines.split('=')[1]
+            reg = re.compile(r',{"nd.*?}')
+            lines = re.subn(reg, '', lines)
+            reg = re.compile(r',"qt":{.*?}')
+            lines = re.subn(reg, '', lines[0])
+            reg = r',"mx_price".*?"version":"4"'
+            lines = re.subn(reg, '', lines[0])
+            reg = r',"mx_price".*?"version":"12"'
+            lines = re.subn(reg, '', lines[0])
+            # 将str格式转换成byte
+            textByte = bytes(lines[0], encoding='utf-8')
+        return textByte
+        raise IOError(ct.NETWORK_URL_ERROR_MSG)
 
 
 def RunGetDayData(date,filetype,count,list_fq):
