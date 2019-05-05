708e8bd84da77729d79a2cd9685def830368ef83
redmarss <31237613+redmarss@users.noreply.github.com>
2019-05-05 23:13:38 +0800

    home

--- Run/DailyToMVN.py
+++ Run/DailyToMVN.py
@@ -89,6 +89,6 @@ if __name__ == '__main__':
 
     #everyday = start.replace("-","")
     #每日获取股票相关数据
-    RunGetDayData(start="2019-01-01",end=end)
+    #RunGetDayData(start="2019-01-01",end=end)
     #每日获取机构数据
     brokerInfo(start,end)
