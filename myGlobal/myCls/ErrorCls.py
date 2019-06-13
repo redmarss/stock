ef64b14a63ce7d8dc298352e11f084ce39242a76
redmarss <31237613+redmarss@users.noreply.github.com>
2019-06-13 16:36:15 +0800

    company

--- myGlobal/myCls/ErrorCls.py
+++ myGlobal/myCls/ErrorCls.py
@@ -1,44 +1,48 @@
 #!/bin/usr/env python
 # -*-coding:utf8 -*-
+from myGlobal.myCls.msqlHelper import DBHelper
 
 
 #自定义错误类，处理其余类可能出现的错误
 class StockError(object):
-    def __init__(self, msg):
-        self.__msg = msg
-        print(msg)
+    def __init__(self, code,ts_date,msg):
+        self._code = code
+        self._tsdate = ts_date
+        self._msg = msg
 
     @property
     def code(self):
-        pass
+        return self._code
 
     @property
     def ts_date(self):
-        pass
+        return self._ts_date
 
     @property
     def name(self):
-        pass
+        sql = f'select stockname from stock_basic_table where stockcode="{self._code}"'
+        t = DBHelper().fetchone(sql)[0]
+        return t
 
     @property
     def open_price(self):
-        pass
+        return 0.00
 
     @property
     def close_price(self):
-        pass
+        return 0.00
 
     @property
     def high_price(self):
-        pass
+        return 0.00
 
     @property
     def low_price(self):
-        pass
+        return 0.00
 
     @property
     def volume(self):
-        pass
+        return 0.00
 
 
 
