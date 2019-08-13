dee7042c8da91a2be85a306459f01d9fa945593c
redmarss <31237613+redmarss@users.noreply.github.com>
2019-08-13 23:29:42 +0800

    home

--- myGlobal/ftypeOperate.py
+++ myGlobal/ftypeOperate.py
@@ -8,21 +8,9 @@
 import myGlobal.globalFunction as gf
 from myGlobal.myCls.msql import DBHelper
 
-TYPE_LEN = 16
+FTYPE_LEN = 16
 
-def list_to_bin(list1):
-    if len(set(list1))==2:                          #如果list1中有两个元素，则必须是0和1
-        if '1' not in list1 or '0' not in list1:
-            return
-    if len(set(list1))>=3:                          #如果list1中有三个不同的元素，则报错
-        return
-    result = 0
-    for i in range(len(list1)):
-        result = result + list1[len(list1)-i-1]*(2**len(list1)-1-i)
-    return result
-
-
-def set_bit(value, index,key=1):
+def set_ftype(value, index, key=1):
     '''
     将value转换二进制并将从右起第index位设置为key
     :param value: 原始值
@@ -30,16 +18,13 @@ def set_bit(value, index,key=1):
     :param key: 0或1
     :return: 返回值
     '''
-    global TYPE_LENLEN
-    if index>TYPE_LEN or index < 1:
-        print(f"index参数超过{TYPE_LEN}，本函数只支持{TYPE_LEN}位,最小不能低于1")
+    if index > FTYPE_LEN or index < 1:
+        print(f"index参数超过{FTYPE_LEN}，本函数只支持{FTYPE_LEN}位,最小不能低于1")
         return value
-    if not 0<=value<=65535:
+    if not 0 <= value <= 65535:
         print("value值必须是0-65535之间")
         return value
-    #判断value的index位是否等于key，如果是，则直接返回value，如果不是，则加上1<<(index-1)
-    list_bin = list(bin(value)[2:].zfill(16))
-    if list_bin[len(list_bin)-index] == str(key):
+    if judgeftype(value,index,key):
         return value
     else:
         if key == 1:
@@ -52,9 +37,7 @@ def set_bit(value, index,key=1):
             print("key值只能为0或1")
             return value
 
-
-
-def _getSimulateFlag(brokercode,stockcode,ts_date):
+def get_ftype(brokercode,stockcode,ts_date):
     '''
     从机构购买股票信息中获取ftype值，以判断是否模拟计算过
     :param brokercode: 机构代码
@@ -76,7 +59,35 @@ def _getSimulateFlag(brokercode,stockcode,ts_date):
     except:
         return -2                       #数据库语句出错，返回-2
 
-def _setSimulaFlag(brokercode,stockcode,ts_date,ftype):
-    flag = _getSimulateFlag(brokercode,stockcode,ts_date)
+def update_ftype(brokercode,stockcode,ts_date,value):
+    stockcode = gf.symbol_to_sqlcode(stockcode)
+    sql = f'''
+    UPDATE broker_buy_stock_info AS a
+    INNER JOIN
+    broker_buy_summary AS b ON a.broker_buy_summary_id = b.id 
+    SET 
+    a.simulate_flag = {value}
+    WHERE
+    stock_code = '{stockcode}'
+    AND broker_code = '{brokercode}'
+    AND ts_date = '{ts_date}';
+    '''
+    try:
+        DBHelper().execute(sql)
+    except:
+        print("sql错误")
 
-print(set_bit(1111111111111111117,3,0))
+def judgeftype(value,index,key=1):
+    # if list_bin[len(list_bin)-index] == str(key):
+    #     return value
+    if index > FTYPE_LEN or index < 1:
+        print(f"index参数超过{FTYPE_LEN}，本函数只支持{FTYPE_LEN}位,最小不能低于1")
+        return False
+    if not 0 <= value <= 65535:
+        print("value值必须是0-65535之间")
+        return False
+    list_bin = list(bin(value)[2:].zfill(FTYPE_LEN))
+    if list_bin[len(list_bin) - index] == str(key):
+        return True
+    else:
+        return False
